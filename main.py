44a97a540bc8888b2c4d8fdd26f4dbebbf320ad1
Tyler Bouknight <git@bluweb.org>
2024-02-26 17:20:22 -0500

    maintain window size

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class PhotoFilter:
         self.master = master
         self.directory = directory
 
+        # Set a fixed window size
+        self.master.geometry('800x600')
+
         # Filter out directories
         self.photos = [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
         self.photos = iter(self.photos)
@@ -24,14 +27,13 @@ class PhotoFilter:
         os.makedirs(os.path.join(self.directory, 'trash'), exist_ok=True)
         os.makedirs(os.path.join(self.directory, 'keep'), exist_ok=True)
 
-
     def next_photo(self):
         try:
             self.photo = next(self.photos)
             image = Image.open(os.path.join(self.directory, self.photo))
 
             # Resize image to fit the window
-            max_size = (self.master.winfo_screenwidth(), self.master.winfo_screenheight())
+            max_size = (800, 600)  # Adjust to your desired window size
             image.thumbnail(max_size, Image.LANCZOS)
 
             photo = ImageTk.PhotoImage(image)
