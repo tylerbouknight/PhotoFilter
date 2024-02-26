da4875be82b7e64f21d9a4d13e9c8d3ca437f44d
Tyler Bouknight <git@bluweb.org>
2024-02-26 17:17:48 -0500

    update main

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+from tkinter import *
+from PIL import Image, ImageTk
+
+class PhotoFilter:
+    def __init__(self, master, directory):
+        self.master = master
+        self.directory = directory
+
+        # Filter out directories
+        self.photos = [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
+        self.photos = iter(self.photos)
+
+        self.photo = None
+        self.label = Label(self.master)
+        self.label.pack()
+        self.master.bind('<Left>', self.move_to_trash)
+        self.master.bind('<Right>', self.keep)
+        self.master.bind('<Up>', self.skip)
+        self.next_photo()
+
+        # Create folders if they don't exist
+        os.makedirs(os.path.join(self.directory, 'trash'), exist_ok=True)
+        os.makedirs(os.path.join(self.directory, 'keep'), exist_ok=True)
+
+
+    def next_photo(self):
+        try:
+            self.photo = next(self.photos)
+            image = Image.open(os.path.join(self.directory, self.photo))
+
+            # Resize image to fit the window
+            max_size = (self.master.winfo_screenwidth(), self.master.winfo_screenheight())
+            image.thumbnail(max_size, Image.LANCZOS)
+
+            photo = ImageTk.PhotoImage(image)
+            self.label.config(image=photo)
+            self.label.image = photo
+        except StopIteration:
+            self.master.quit()
+
+    def move_to_trash(self, event):
+        shutil.move(os.path.join(self.directory, self.photo), os.path.join(self.directory, 'trash', self.photo))
+        self.next_photo()
+
+    def keep(self, event):
+        shutil.move(os.path.join(self.directory, self.photo), os.path.join(self.directory, 'keep', self.photo))
+        self.next_photo()
+
+    def skip(self, event):
+        self.next_photo()
+
+root = Tk()
+app = PhotoFilter(root, 'C:/Users/Tyler/Pictures/Found - Copy')
+root.mainloop()
