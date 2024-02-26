dffb88646fb0aa0a7843fb5e14c4be4312b28c96
Tyler Bouknight <git@bluweb.org>
2024-02-26 18:43:10 -0500

    Refactor main.py to use PyQt5 for GUI

--- main.py
+++ main.py
@@ -1,86 +1,9 @@
-import os
-import shutil
-from tkinter import *
-from PIL import Image, ImageTk
+from PyQt5.QtWidgets import QApplication
+from photo_filter import PhotoFilter
 
-class PhotoFilter:
-    def __init__(self, master, directory):
-        self.master = master
-        self.directory = directory
-        self.action_stack = []
-
-        # Set a fixed window size
-        self.master.geometry('800x600')
-
-        # Filter out directories
-        self.photos = [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
-        self.current_photo_index = 0
-
-        self.photo = None
-        self.label = Label(self.master)
-        self.label.pack()
-        self.master.bind('<Left>', self.move_to_trash)
-        self.master.bind('<Right>', self.keep)
-        self.master.bind('<Up>', self.skip)
-
-        self.menu = Menu(self.master)
-        self.master.config(menu=self.menu)
-
-        # Create 'Edit' menu
-        self.edit_menu = Menu(self.menu)
-        self.menu.add_cascade(label='Edit', menu=self.edit_menu)
-        self.edit_menu.add_command(label='Undo', command=self.undo)
-
-        # Bind Ctrl+Z to undo
-        self.master.bind_all('<Control-z>', lambda event: self.undo())
-
-        # Create folders if they don't exist
-        os.makedirs(os.path.join(self.directory, 'trash'), exist_ok=True)
-        os.makedirs(os.path.join(self.directory, 'keep'), exist_ok=True)
-
-        self.next_photo()
-
-    def next_photo(self):
-        try:
-            self.photo = self.photos[self.current_photo_index]
-            self.current_photo_index += 1
-            self.display_photo()
-        except IndexError:
-            self.master.quit()
-
-    
-    def display_photo(self):
-        image = Image.open(os.path.join(self.directory, self.photo))
-
-        # Resize image to fit the window
-        max_size = (800, 600)  # Adjust to your desired window size
-        image.thumbnail(max_size, Image.LANCZOS)
-
-        photo = ImageTk.PhotoImage(image)
-        self.label.config(image=photo)
-        self.label.image = photo
-
-    def undo(self):
-        if self.action_stack:
-            last_action, last_photo = self.action_stack.pop()
-            shutil.move(os.path.join(self.directory, last_action, last_photo), os.path.join(self.directory, last_photo))
-            self.photo = last_photo
-            self.current_photo_index -= 1
-            self.display_photo()
-            
-    def move_to_trash(self, event):
-        shutil.move(os.path.join(self.directory, self.photo), os.path.join(self.directory, 'trash', self.photo))
-        self.action_stack.append(('trash', self.photo))
-        self.next_photo()
-
-    def keep(self, event):
-        shutil.move(os.path.join(self.directory, self.photo), os.path.join(self.directory, 'keep', self.photo))
-        self.action_stack.append(('keep', self.photo))
-        self.next_photo()
-
-    def skip(self, event):
-        self.next_photo()
-
-root = Tk()
-app = PhotoFilter(root, 'C:/Users/Tyler/Pictures/Found - Copy')
-root.mainloop()
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    directory = 'C:/Users/Tyler/Pictures/Found - Copy'
+    ex = PhotoFilter(directory)
+    sys.exit(app.exec_())
---
+++ photo_filter.py
@@ -0,0 +1,88 @@
+import os
+import shutil
+from PyQt5.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget, QAction, QMenu
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt
+
+class PhotoFilter(QMainWindow):
+    def __init__(self, directory):
+        super().__init__()
+        self.setWindowTitle('Photo Filter')
+        self.directory = directory
+        self.action_stack = []
+
+        # Filter out directories
+        self.photos = [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
+        self.current_photo_index = 0
+
+        self.photo = None
+        self.label = QLabel(self)
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.menu = self.menuBar()
+        self.edit_menu = QMenu('Edit', self)
+        self.undo_action = QAction('Undo', self)
+        self.undo_action.setShortcut('Ctrl+Z')
+        self.undo_action.triggered.connect(self.undo)
+        self.edit_menu.addAction(self.undo_action)
+        self.menu.addMenu(self.edit_menu)
+
+        # Create folders if they don't exist
+        os.makedirs(os.path.join(self.directory, 'trash'), exist_ok=True)
+        os.makedirs(os.path.join(self.directory, 'keep'), exist_ok=True)
+
+        self.next_photo()
+
+        # Set a fixed window size
+        self.setFixedSize(800, 600)
+
+        self.show()
+
+
+    def next_photo(self):
+        try:
+            self.photo = self.photos[self.current_photo_index]
+            self.current_photo_index += 1
+            self.display_photo()
+        except IndexError:
+            self.close()
+
+    def display_photo(self):
+        image_path = os.path.join(self.directory, self.photo)
+        image = QImage(image_path)
+        pixmap = QPixmap.fromImage(image)
+        self.label.setPixmap(pixmap.scaled(800, 600, Qt.KeepAspectRatio))
+        self.label.setAlignment(Qt.AlignCenter)
+
+    def undo(self):
+        if self.action_stack:
+            last_action, last_photo = self.action_stack.pop()
+            shutil.move(os.path.join(self.directory, last_action, last_photo), os.path.join(self.directory, last_photo))
+            self.photo = last_photo
+            self.current_photo_index -= 1
+            self.display_photo()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Left:
+            self.move_to_trash()
+        elif event.key() == Qt.Key_Right:
+            self.keep()
+        elif event.key() == Qt.Key_Up:
+            self.skip()
+
+    def move_to_trash(self):
+        shutil.move(os.path.join(self.directory, self.photo), os.path.join(self.directory, 'trash', self.photo))
+        self.action_stack.append(('trash', self.photo))
+        self.next_photo()
+
+    def keep(self):
+        shutil.move(os.path.join(self.directory, self.photo), os.path.join(self.directory, 'keep', self.photo))
+        self.action_stack.append(('keep', self.photo))
+        self.next_photo()
+
+    def skip(self):
+        self.next_photo()
